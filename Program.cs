53851bf69dd696d4406df6c024653ef9b38f64d3
DrTau <drttau@gmail.com>
2021-10-29 16:58:02 +0300

    Added output with depth

--- Program.cs
+++ Program.cs
@@ -8,26 +8,50 @@ namespace miniTerminal2
     class Program
     {
         private static DirectoryInfo dir;
-        private static int depth;
+        private static int globalDepth;
+        private static string line = "    ";
+        private static string lineStart = "└───";
+
+        static void printSpaces(int count)
+        {
+            for (int i = 0; i < count - 2; i++)
+            {
+                Console.Write(line);
+            }
+            if (count != 1) Console.Write(lineStart);
+        }
+
+        static void printingFiles(DirectoryInfo dir, int depth)
+        {
+            depth++;
+
+            foreach (var dirInfo in dir.GetDirectories())
+            {
+                printSpaces(depth);
+                Console.WriteLine("[" + dirInfo.Name + "]");
+                if (depth < globalDepth)
+                {
+                    printingFiles(dirInfo, depth);
+                }
+            }
+
+            foreach (var fileInfo in dir.GetFiles())
+            {
+                printSpaces(depth);
+                Console.WriteLine(fileInfo.Name + " (" + (fileInfo.Length / 8) + " B)");
+            }
+
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter directory path: ");
+            Console.Write("Enter directory path: ");
             dir = new DirectoryInfo(Console.ReadLine());
             if (!dir.Exists) throw new ArgumentException("Incorrect directory");
-            Console.WriteLine("Enter depth: ");
-            depth = (int)Int32.Parse(Console.ReadLine());
+            Console.Write("Enter depth: ");
+            globalDepth = (int)Int32.Parse(Console.ReadLine());
 
-            foreach (var dirInfo in currentDirectory.GetDirectories())
-            {
-                Console.WriteLine("[" + dirInfo.FullName + "]");
-            }
-
-            foreach (var file in dir.GetFiles())
-            {
-                Console.WriteLine(file.Name);
-                //file.Length;
-            }
-            Console.WriteLine("Hello World!");
+            printingFiles(dir, 0);
         }
     }
 }
